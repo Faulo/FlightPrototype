5c4e944cbfad3ecd6aca53bfd019951b99efb261
Daniel Schulz <faulolio@gmail.com>
2020-05-15 14:54:37 +0200

    Make space the jump key

--- Assets/Scripts/AvatarInput.cs
+++ Assets/Scripts/AvatarInput.cs
@@ -3,14 +3,24 @@
 public class AvatarInput : MonoBehaviour {
     [SerializeField]
     Avatar avatar = default;
+
+    [SerializeField]
+    string horizontalAxis = "Horizontal";
+    [SerializeField]
+    string verticalAxis = "Vertical";
+    [SerializeField]
+    string jumpButton = "Fire1";
+
     void Update() {
-        avatar.intendedMovement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-        avatar.intendsJump = Input.anyKey;
+        avatar.intendedMovement = new Vector2(Input.GetAxis(horizontalAxis), Input.GetAxis(verticalAxis));
+        avatar.intendsJump = Input.GetButton(jumpButton);
         if (avatar.isAirborne) {
-            avatar.intendsGlide = Input.anyKeyDown;
+            if (Input.GetButtonDown(jumpButton)) {
+                avatar.intendsGlide = true;
+            }
         }
         if (avatar.isGliding) {
-            avatar.intendsGlide = Input.anyKey;
+            avatar.intendsGlide = Input.GetButton(jumpButton);
         }
     }
 }
