058b9d16fc62bf44e34842a32022b27dc0a8fb60
Daniel Schulz <faulolio@gmail.com>
2020-05-27 18:06:55 +0200

    Add debug UI

---
+++ Assets/Scripts/UI/DebugInformation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UI {
+    public class DebugInformation : FormattableText {
+        public override IDictionary<string, string> parameters {
+            get {
+                return new Dictionary<string, string> {
+                    ["Scene.name"] = SceneManager.GetActiveScene().name,
+                    ["Scene.path"] = SceneManager.GetActiveScene().path,
+                    ["Assembly.version"] = Assembly.GetExecutingAssembly().GetName().Version.ToString(),
+                    ["Assembly.build"] = Assembly.GetExecutingAssembly().GetName().Version.Build.ToString(),
+                    ["Assembly.revision"] = Assembly.GetExecutingAssembly().GetName().Version.Revision.ToString(),
+                    ["Debug.FPS"] = Time.smoothDeltaTime == 0 ? "???" : Mathf.RoundToInt(1 / Time.smoothDeltaTime).ToString(),
+                };
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/UI/FormattableText.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Extensions;
+using TMPro;
+using UnityEngine;
+
+namespace UI {
+    [RequireComponent(typeof(TextMeshProUGUI))]
+    public abstract class FormattableText : MonoBehaviour {
+        [SerializeField, Range(0, 1)]
+        float refreshInterval = 1;
+
+        public abstract IDictionary<string, string> parameters { get; }
+
+        IEnumerator Start() {
+            var textMesh = GetComponent<TextMeshProUGUI>();
+            string template = textMesh.text;
+            while (true) {
+                textMesh.text = template.Format(parameters);
+                yield return new WaitForSeconds(refreshInterval);
+            }
+        }
+
+        public void ToggleDisplay() {
+            GetComponent<TextMeshProUGUI>().enabled = !GetComponent<TextMeshProUGUI>().enabled;
+        }
+    }
+}
