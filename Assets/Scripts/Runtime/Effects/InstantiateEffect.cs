999417a9feaaa931b9edc5204574b3b6dab0df04
Daniel Schulz <faulolio@gmail.com>
2020-06-17 01:39:25 +0200

    Introduce InstantiateEffect

---
+++ Assets/Scripts/Runtime/Effects/InstantiateEffect.cs
@@ -0,0 +1,15 @@
+﻿using Slothsoft.UnityExtensions;
+using UnityEngine;
+
+namespace TheCursedBroom.Effects {
+    [CreateAssetMenu()]
+    public class InstantiateEffect : Effect {
+        [SerializeField, Expandable]
+        GameObject[] prefabs = default;
+
+        public override void Invoke(GameObject context) {
+            var prefab = prefabs.RandomElement();
+            Instantiate(prefab, context.transform.position, prefab.transform.rotation);
+        }
+    }
+}
