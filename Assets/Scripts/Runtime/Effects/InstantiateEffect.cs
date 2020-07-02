617b2174414e28399536946da05c66a908f40f1f
Daniel Schulz <faulolio@gmail.com>
2020-07-02 12:37:55 +0200

    Tweak save particles

--- Assets/Scripts/Runtime/Effects/InstantiateEffect.cs
+++ Assets/Scripts/Runtime/Effects/InstantiateEffect.cs
@@ -6,10 +6,16 @@ namespace TheCursedBroom.Effects {
     public class InstantiateEffect : Effect {
         [SerializeField, Expandable]
         GameObject[] prefabs = default;
+        [SerializeField]
+        bool destroyPreviousInstance = false;
+        GameObject instance;
 
         public override void Invoke(GameObject context) {
             var prefab = prefabs.RandomElement();
-            Instantiate(prefab, context.transform.position, context.transform.rotation);
+            if (destroyPreviousInstance && instance) {
+                Destroy(instance);
+            }
+            instance = Instantiate(prefab, context.transform.position, context.transform.rotation);
         }
     }
 }
