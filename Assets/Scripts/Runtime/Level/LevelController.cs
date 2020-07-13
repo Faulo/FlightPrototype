fa610c9280b53004e85796230ce2eabfc13e3a18
Daniel Schulz <faulolio@gmail.com>
2020-07-13 18:36:51 +0200

    Start work on wrapping level

--- Assets/Scripts/Runtime/Level/LevelController.cs
+++ Assets/Scripts/Runtime/Level/LevelController.cs
@@ -1,13 +1,57 @@
-﻿using UnityEngine;
+﻿using Slothsoft.UnityExtensions;
+using UnityEngine;
+using UnityEngine.Tilemaps;
 
 namespace TheCursedBroom.Level {
     public class LevelController : MonoBehaviour {
+        [Header("MonoBehaviour configuration")]
+        [SerializeField, Expandable]
+        Tilemap background = default;
+        [SerializeField, Expandable]
+        Tilemap ground = default;
+        [SerializeField, Expandable]
+        Tilemap objects = default;
+        [SerializeField, Expandable]
+        Tilemap decorations = default;
+
+        [Header("Levels")]
+        [SerializeField, Expandable]
+        TilemapChunk[] levels = new TilemapChunk[0];
+
         [Header("Events")]
         [SerializeField]
         GameObjectEvent onStart = default;
 
         void Start() {
+            var level = levels[0];
+
+            var offset = new Vector3Int(level.size.x, 0, 0);
+
+            foreach (var position in level.tilePositions) {
+                if (level.ground.HasTile(position)) {
+                    ground.SetTile(position - offset, level.ground.GetTile(position));
+                    ground.SetTile(position, level.ground.GetTile(position));
+                    ground.SetTile(position + offset, level.ground.GetTile(position));
+                }
+            }
+
+
             onStart.Invoke(gameObject);
+        }
+
+        void OnValidate() {
+            if (!background) {
+                background = GetComponentsInChildren<Tilemap>()[0];
+            }
+            if (!ground) {
+                ground = GetComponentsInChildren<Tilemap>()[1];
+            }
+            if (!objects) {
+                objects = GetComponentsInChildren<Tilemap>()[2];
+            }
+            if (!decorations) {
+                decorations = GetComponentsInChildren<Tilemap>()[3];
+            }
         }
     }
 }
---
+++ Assets/Scripts/Runtime/Level/TilemapChunk.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using Slothsoft.UnityExtensions;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace TheCursedBroom.Level {
+    public class TilemapChunk : MonoBehaviour {
+        [Header("MonoBehaviour configuration")]
+        [SerializeField, Expandable]
+        public Tilemap background = default;
+        [SerializeField, Expandable]
+        public Tilemap ground = default;
+        [SerializeField, Expandable]
+        public Tilemap objects = default;
+        [SerializeField, Expandable]
+        public Tilemap decorations = default;
+
+        [Header("Level configuration")]
+        [SerializeField, Range(1, 1000)]
+        int width = 100;
+        [SerializeField, Range(1, 1000)]
+        int height = 50;
+
+        public Vector2Int size => new Vector2Int(width, height);
+
+        public IEnumerable<Vector3Int> tilePositions {
+            get {
+                for (int x = 0; x < width; x++) {
+                    for (int y = 0; y < height; y++) {
+                        yield return new Vector3Int(x, y, 0);
+                    }
+                }
+            }
+        }
+
+        Vector3 worldBottomLeft => background.GetCellCenterWorld(Vector3Int.zero);
+
+        void OnValidate() {
+            if (!background) {
+                background = GetComponentsInChildren<Tilemap>()[0];
+            }
+            if (!ground) {
+                ground = GetComponentsInChildren<Tilemap>()[1];
+            }
+            if (!objects) {
+                objects = GetComponentsInChildren<Tilemap>()[2];
+            }
+            if (!decorations) {
+                decorations = GetComponentsInChildren<Tilemap>()[3];
+            }
+        }
+
+        void OnDrawGizmos() {
+            Gizmos.color = Color.white;
+            var size = (Vector3)(Vector2)this.size;
+            Gizmos.DrawWireCube(worldBottomLeft + size / 2, size + Vector3.one);
+        }
+    }
+}
