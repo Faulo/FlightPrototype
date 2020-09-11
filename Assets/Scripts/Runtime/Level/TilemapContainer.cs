fecc2a44c2d2eb2d8af8e132d29557500c89d79b
Daniel Schulz <faulolio@gmail.com>
2020-09-11 14:36:20 +0200

    Format code

--- Assets/Scripts/Runtime/Level/TilemapContainer.cs
+++ Assets/Scripts/Runtime/Level/TilemapContainer.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using Slothsoft.UnityExtensions;
 using UnityEngine;
 using UnityEngine.Assertions;
 using UnityEngine.Tilemaps;
