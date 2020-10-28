23b2f0324a9bf7b8015a74b5520b382f6117bf7f
Daniel Schulz <faulolio@gmail.com>
2020-10-28 16:18:50 +0100

    Implement bgm cross fade

--- Assets/Scripts/Runtime/AudioManager.cs
+++ Assets/Scripts/Runtime/AudioManager.cs
@@ -8,12 +8,18 @@ namespace TheCursedBroom {
 
         [SerializeField, Expandable]
         AudioSource sourcePrefab = default;
+        [SerializeField]
+        AnimationCurve fadeOutCurve = default;
+        [SerializeField]
+        AnimationCurve fadeInCurve = default;
+
+        AudioSource musicSource;
 
         void OnEnable() {
             instance = this;
         }
 
-        public void Play(AudioInfo audio) {
+        public AudioSource InstantiateAudioSource(AudioInfo audio) {
             var source = Instantiate(sourcePrefab, audio.position, Quaternion.identity, transform);
             source.loop = audio.loop;
             source.clip = audio.clip;
@@ -21,8 +27,34 @@ namespace TheCursedBroom {
             source.spatialBlend = audio.spatialBlend;
             source.pitch = audio.pitch;
             source.time = audio.timeOffset;
-            source.Play();
+            return source;
+        }
 
+        public void PlayMusic(AudioInfo audio, float crossFadeDuration) {
+            if (musicSource) {
+                audio.timeOffset = musicSource.time;
+                StartCoroutine(CreateFadeRoutine(musicSource, fadeOutCurve, crossFadeDuration));
+                Destroy(musicSource.gameObject, crossFadeDuration);
+            }
+            musicSource = InstantiateAudioSource(audio);
+            musicSource.Play();
+            StartCoroutine(CreateFadeRoutine(musicSource, fadeInCurve, crossFadeDuration));
+        }
+        IEnumerator CreateFadeRoutine(AudioSource source, AnimationCurve curve, float duration) {
+            for (float timer = 0; timer < duration; timer += Time.deltaTime) {
+                if (source) {
+                    source.volume = curve.Evaluate(timer / duration);
+                }
+                yield return null;
+            }
+            if (source) {
+                source.volume = curve.Evaluate(1);
+            }
+        }
+
+        public void PlaySFX(AudioInfo audio) {
+            var source = InstantiateAudioSource(audio);
+            source.Play();
             if (audio.playDuration > 0) {
                 StartCoroutine(CreateMuteRoutine(source, audio.playDuration));
             }
