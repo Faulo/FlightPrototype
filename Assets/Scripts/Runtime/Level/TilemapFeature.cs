c922739342992deaed031d81892b7f42711977cd
Daniel Schulz <faulolio@gmail.com>
2020-11-07 00:58:50 +0100

    Format code

--- Assets/Scripts/Runtime/Level/TilemapFeature.cs
+++ Assets/Scripts/Runtime/Level/TilemapFeature.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Tilemaps;
-
 namespace TheCursedBroom.Level {
     public abstract class TilemapFeature : ComponentFeature<TilemapController> {
     }
